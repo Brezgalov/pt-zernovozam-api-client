345655ab70dac683f7ebfef32ae453f5bde23a72
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-11-27 21:25:23 +0300

    use timeslot dto instead of window dto

--- src/ResponseAdapters/GetWindowsResponseAdapter.php
+++ src/ResponseAdapters/GetWindowsResponseAdapter.php
@@ -3,7 +3,7 @@
 namespace Brezgalov\ZernovozamApiClient\ResponseAdapters;
 
 use Brezgalov\BaseApiClient\ResponseAdapters\BaseResponseAdapter;
-use Brezgalov\ZernovozamApiClient\Dto\WindowDto;
+use Brezgalov\ZernovozamApiClient\Dto\TimeslotDto;
 use Brezgalov\ZernovozamApiClient\ZernovozamApiClient;
 use yii\base\InvalidConfigException;
 
@@ -32,29 +32,16 @@ class GetWindowsResponseAdapter extends BaseResponseAdapter
     }
 
     /**
-     * @return WindowDto[]
+     * @return TimeslotDto[]
      * @throws InvalidConfigException
      */
-    public function getWidowsArray()
+    public function getTimeslotsArray()
     {
         $windows = $this->responseData['Windows'] ?? [];
 
         $result = [];
         foreach ($windows as $window) {
-            $result[] = \Yii::createObject(WindowDto::class, [
-                'cultureId' => $window['CultureId'] ?? null,
-                'time' => array_key_exists('Date', $window) ? strtotime($window['Date']) : null,
-                'timeEnd' => array_key_exists('DateEnd', $window) ? strtotime($window['DateEnd']) : null,
-                'timeStart' => array_key_exists('DateStart', $window) ? strtotime($window['DateStart']) : null,
-                'timeslotId' => $window['Id'] ??  null,
-                'IsArrived' => $window['IsArrived'] ?? null,
-                'IsLate' => $window['IsLate'] ?? null,
-                'IsOwnWindow' => $window['IsOwnWindow'] ?? null,
-                'ReceiverId' => $window['ReceiverId'] ?? null,
-                'ShowPhone' => $window['ShowPhone'] ?? null,
-                'TraderId' => $window['TraderId'] ?? null,
-                'ProviderId' => $window['ProviderId'] ?? null,
-            ]);
+            $result[] = \Yii::createObject(TimeslotDto::class, $window);
         }
 
         return $result;
